bea9a402b58ed39c196b93eed84273f30aa2c01f
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-10 09:01:48 -0400

    added ngrx feature module for auth module

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,11 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer,
+} from "@ngrx/store";
+
+export interface State {}
+
+export const reducers: ActionReducerMap<State> = {};
