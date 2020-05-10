2229d56c2dd7787dcfcc34432458eceb4b9ac981
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-10 10:07:24 -0400

    completed auth reducer

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -39,8 +39,10 @@ export class LoginComponent implements OnInit {
       .pipe(
         tap((user) => {
           console.log(user);
+          const newLoginAction = login({ user });
+          console.log("New Login Action:", newLoginAction);
           // store the user profile to the store
-          this.store.dispatch(login({ user }));
+          this.store.dispatch(newLoginAction);
           this.router.navigateByUrl("/courses");
         })
       )
--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -4,8 +4,24 @@ import {
   createFeatureSelector,
   createSelector,
   MetaReducer,
+  createReducer,
+  on,
 } from "@ngrx/store";
+import { User } from "../model/user.model";
+import { AuthActions } from "../action-types";
 
-export interface State {}
+export interface AuthState {
+  user: User;
+}
 
-export const reducers: ActionReducerMap<State> = {};
+export const initialAuthState: AuthState = { user: undefined };
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    console.log("Calling login reducer");
+    return {
+      user: action.user,
+    };
+  })
+);
